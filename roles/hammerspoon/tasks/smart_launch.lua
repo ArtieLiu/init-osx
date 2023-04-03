98e5a597b15d60b56e58d1e06b2c522c44699f72
ArtieLiu <yinan97@outlook.com>
2023-04-03 22:04:57 +0800

    increase rest time to 5 minutes

--- roles/hammerspoon/tasks/smart_launch.lua
+++ roles/hammerspoon/tasks/smart_launch.lua
@@ -4,7 +4,7 @@ log = hs.logger.new("smart_launch", "debug")
 sec = 1
 min = 60 * sec
 SESSION_LENGTH = 25 * min
-BREAK_DURATION = 3 * min
+BREAK_DURATION = 5 * min
 -- ------------------
 
 LAST_BREAK_TIME = os.time()
@@ -12,7 +12,7 @@ LAST_BREAK_TIME = os.time()
 function launch(name)
     return function()
         if is_time_to_rest() then
-            show_message_to_rest()
+            prompt_to_rest()
         end
         hs.application.launchOrFocus(name)
     end
@@ -30,7 +30,7 @@ function is_time_to_rest()
 
 end
 
-function show_message_to_rest()
+function prompt_to_rest()
     hs.sound.getByName("Glass"):play()
     hs.alert.show("Break", {
         fillColor = { red = 181/255, green = 100/255, blue = 100/255, alpha=1},
@@ -41,5 +41,5 @@ end
 function force_break()
     log.d("forcing a break at: " .. os.date("%H:%M", LAST_BREAK_TIME))
     LAST_BREAK_TIME = os.time() - SESSION_LENGTH - 1 * min
-    show_message_to_rest()
+    prompt_to_rest()
 end
