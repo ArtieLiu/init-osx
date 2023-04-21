244f5eb4c6731e9dd1abb45f55043f7877a14e31
ArtieLiu <yinan97@outlook.com>
2023-04-21 11:43:53 +0800

    prevent break popup from showing if it is the first session of the day or morning or afternoon or whatsoever

--- roles/hammerspoon/tasks/smart_launch.lua
+++ roles/hammerspoon/tasks/smart_launch.lua
@@ -3,6 +3,7 @@ log = hs.logger.new("smart_launch", "debug")
 -- time helpers
 sec = 1
 min = 60 * sec
+hour = 60 * min
 SESSION_LENGTH = 25 * min
 BREAK_DURATION = 5 * min
 -- ------------------
@@ -11,18 +12,26 @@ LAST_BREAK_TIME = os.time()
 
 function launch(name)
     return function()
-        if is_time_to_rest() then
+        if last_break_is_one_session_ago() then
             prompt_to_rest()
         end
         hs.application.launchOrFocus(name)
     end
 end
 
-function is_time_to_rest()
-    now = os.time()
+function last_break_is_one_session_ago()
+    local now = os.time()
+    local function is_first_session()
+        return now - LAST_BREAK_TIME > 2 * hour
+    end
+
+    if is_first_session() then
+        LAST_BREAK_TIME = now
+        return false
+    end
     if now - LAST_BREAK_TIME > SESSION_LENGTH then
         LAST_BREAK_TIME = now
-        log.d("breat at: " .. os.date("%H:%M", LAST_BREAK_TIME))
+        log.d("break at: " .. os.date("%H:%M", LAST_BREAK_TIME))
         return true
     else
         return false
@@ -33,7 +42,7 @@ end
 function prompt_to_rest()
     hs.sound.getByName("Glass"):play()
     hs.alert.show("Break", {
-        fillColor = { red = 181/255, green = 100/255, blue = 100/255, alpha=1},
+        fillColor = { red = 181 / 255, green = 100 / 255, blue = 100 / 255, alpha = 1 },
         fadeOutDuration = BREAK_DURATION,
     })
 end
