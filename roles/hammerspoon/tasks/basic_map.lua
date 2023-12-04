60592361e0fb17d0fe7a3c894251887ed72057f8
ArtieLiu <yinan97@outlook.com>
2023-12-04 20:51:41 +0800

    remove toggling of clash

--- roles/hammerspoon/tasks/basic_map.lua
+++ roles/hammerspoon/tasks/basic_map.lua
@@ -22,7 +22,6 @@ remap(HYPER     , 'l', pressFn('alt', 'right'))
 
 hs.hotkey.bind(HYPER_LITE, ",", launch("System Settings"))
 hs.hotkey.bind(HYPER_LITE, "f", function() hs.execute("open ~/Downloads") end)
-hs.hotkey.bind(HYPER_LITE, "c", launch("chatgpt"))
 hs.hotkey.bind(HYPER_LITE, "p", launch("IntelliJ IDEA"))
 hs.hotkey.bind(HYPER_LITE, "i", launch("iTerm"))
 hs.hotkey.bind(HYPER_LITE, "o", launch("Google Chrome"))
--- roles/hammerspoon/tasks/init.lua
+++ roles/hammerspoon/tasks/init.lua
@@ -1,9 +1,10 @@
 require("basic_map")
 
+hs.hotkey.bind(HYPER, "b", force_break)
 hs.hotkey.bind(HYPER_LITE, "b", launch("obsidian"))
+--hs.hotkey.bind(HYPER_LITE, "c", launch("chatgpt"))
 hs.hotkey.bind(HYPER_LITE, "d", launch("Collins"))
 hs.hotkey.bind(HYPER_LITE, "m", launch("Music"))
 --hs.hotkey.bind(HYPER_LITE, "r", togglelaunch("ClashX Pro"))
 hs.hotkey.bind(HYPER_LITE, "w", launch("wechat"))
-hs.hotkey.bind(HYPER, "b", force_break)
 --  HYPER_LITE, "4", open("Snipaste")
