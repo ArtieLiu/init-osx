5e391f89d58691d7cbfd6b7febcbf41af181b6bf
Yinan Liu <167047937+Yinan97@users.noreply.github.com>
2024-10-04 12:05:50 +0800

    main: add and clean up

--- roles/hammerspoon/tasks/init.lua
+++ roles/hammerspoon/tasks/init.lua
@@ -8,7 +8,6 @@ hs.hotkey.bind(HYPER_LITE, "o", launch("Google Chrome"))
 hs.hotkey.bind(HYPER_LITE, "n", launch("Sublime Text"))
 
 hs.hotkey.bind(HYPER_LITE, "e", launch("poe"))
-hs.hotkey.bind(HYPER_LITE, "s", launch("surge"))
 hs.hotkey.bind(HYPER_LITE, "m", launch("NetEaseMusic"))
 hs.hotkey.bind(HYPER_LITE, "w", launch("wechat"))
 hs.hotkey.bind(HYPER_LITE, "t", launch("Microsoft Teams"))
