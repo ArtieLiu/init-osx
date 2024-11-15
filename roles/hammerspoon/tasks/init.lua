0fef1bfa6db864cdfb8e234826d80311b5267052
Yinan Liu <167047937+Yinan97@users.noreply.github.com>
2024-11-15 16:02:41 +0800

    main: update settings

--- roles/hammerspoon/tasks/init.lua
+++ roles/hammerspoon/tasks/init.lua
@@ -1,11 +1,14 @@
 require("basic_map")
 
 hs.hotkey.bind(HYPER_LITE, ",", launch("System Settings"))
-hs.hotkey.bind(HYPER_LITE, "f", launch("Finder"))
+hs.hotkey.bind(HYPER_LITE, "f",                function()
+                                                   hs.execute("open ~/Desktop")
+                                               end)
+
 hs.hotkey.bind(HYPER_LITE, "p", launch("IntelliJ IDEA"))
 hs.hotkey.bind(HYPER_LITE, "i", launch("alacritty"))
 hs.hotkey.bind(HYPER_LITE, "o", launch("Google Chrome"))
-hs.hotkey.bind(HYPER_LITE, "n", launch("Sublime Text"))
+hs.hotkey.bind(HYPER_LITE, "n", launch("Visual Studio Code"))
 
 hs.hotkey.bind(HYPER_LITE, "e", launch("poe"))
 hs.hotkey.bind(HYPER_LITE, "m", launch("NetEaseMusic"))
